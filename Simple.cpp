0aa5a1bd275b55209a0dc0cddc4e74a3fbbc44b0
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-02-16 03:25:58 +0530

    Add files via upload

---
+++ Simple.cpp
@@ -0,0 +1,11 @@
+//Simply display the Strings - Hello coder
+
+
+#include<iostream>                       //Used for the i/o operation..
+using namespace std;
+
+int main()                                //Executing start..
+{
+    cout << "Hello Coders" << endl;
+    return 0;                                   //Executing end..
+}
