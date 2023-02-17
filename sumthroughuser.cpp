81076dfff83cb51d0602aefccfeef89f36e02c1e
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-02-17 22:36:17 +0530

    Adding two numbers through user input

---
+++ sumthroughuser.cpp
@@ -0,0 +1,16 @@
+//Adding the p and m value ...
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p,m,sum;
+
+    cout<<"Enter the first integer.."<<endl;
+    cin >> p;
+    cout<<"Enter the second integer.."<<endl;
+    cin >> m;
+    sum = p+m;
+    cout<<"The sum of " <<  p << " and " << m << " is = " <<sum<<endl;
+    return 0;
+
+}
