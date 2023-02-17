e95bc4e8cbec06541bd9ae8c7111970da726688a
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-02-17 22:35:23 +0530

    Normal Adding two integers

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+//display adding the 7 and 5 using varibles and datatypes 
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int p = 5;
+    int m = 7;
+    int sum = p + m;
+
+    cout << "The sum of " << p << " and " << m << " is " << sum <<endl;
+    return 0;
+}
